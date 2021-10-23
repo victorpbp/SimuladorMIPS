2029015148b97651eaa5ae2e47b0ab9e5327a4e7
Victor Peixoto <lebackstab@yahoo.com.br>
2021-10-23 16:06:52 -0300

    Adição do ADDU e mudança de todos para funções porque tava dando erro de compilação porque o switch e escopo da variável não funcionam bem pro que a gente tá fazendo aqui, então pra não declarar um milhão de variáveis diferentes, aí botei em funções kkkkkkkkkkkk

--- source/sim.c
+++ source/sim.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include "shell.h"
 
+void add(uint32_t instruction) {
+
+    uint32_t rs = (instruction<<6)>>27; //Valor do registrador com o primeiro valor de adição
+
+    uint32_t rt = (instruction<<11)>>27; //Valor do registrador com o segundo valor de adição
+
+    uint32_t rd = (instruction<<16)>>27; //Valor do registrador destino da operação
+
+    NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] + CURRENT_STATE.REGS[rt];
+
+}
+
 void process_instruction()
 {
     printf("Aqui na funcao process_instruction\n");
@@ -12,6 +24,11 @@ void process_instruction()
 
      uint32_t opcode = instruction>>26;
 
+     NEXT_STATE.PC = CURRENT_STATE.PC + 4; // To colocando isso aqui pra gente omitir nos outros campos
+     //Aí quando a gente tiver tratando de Jumps e tal, é só levar isso em consideração
+     //Se for jump relativo, a gente sempre tem que contar com o +4, então tudo normal
+     //Se for jump absoluto, tudo bem também porque a gente só tá transferindo o valor e esse primeiro comando vai ser "inútil" mas ele vai facilitar muito o entendimento e evita DEMAIS repetição no código
+
      switch(opcode) {
 
         //Registradores//
@@ -22,11 +39,26 @@ void process_instruction()
 
             switch (functcode) { 
 
-                case (0x20): //add //Causes an exception upon overflow
+                //ADD//
 
-                
+                case (0x20):; //add //Causes an exception upon overflow (How do I do that?)
+
+                add(instruction);
 
 
+                break;
+
+                //ADDU//
+
+                case (0x21):; //addu //Does NOT cause an exception upon overflow (How do I do that?)
+
+                uint32_t rs = (instruction<<6)>>27; //Valor do registrador com o primeiro valor de adição
+
+                uint32_t rt = (instruction<<11)>>27; //Valor do registrador com o segundo valor de adição
+
+                uint32_t rd = (instruction<<16)>>27; //Valor do registrador destino da operação
+
+                NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] + CURRENT_STATE.REGS[rt];
 
 
                 break;
@@ -49,14 +81,14 @@ void process_instruction()
 
             NEXT_STATE.REGS[rt] = CURRENT_STATE.REGS[rs] + imme;
 
-            NEXT_STATE.PC = CURRENT_STATE.PC + 4;
-
-            printf("valor do registrador: %d\n",NEXT_STATE.REGS[2]); //Caso de teste pra saber o valor do V0
-            printf("valor do registrador: %d\n",NEXT_STATE.REGS[8]); //Caso de teste pra saber o valor do t0
-            printf("valor do registrador: %d\n",NEXT_STATE.REGS[9]); //Caso de testa pra saber o valor do t1
+            //printf("valor do registrador: %d\n",NEXT_STATE.REGS[2]); //Caso de teste pra saber o valor do V0
+            //printf("valor do registrador: %d\n",NEXT_STATE.REGS[8]); //Caso de teste pra saber o valor do t0
+            //printf("valor do registrador: %d\n",NEXT_STATE.REGS[9]); //Caso de testa pra saber o valor do t1
 
         break;
 
 
      }
+
+
 }
